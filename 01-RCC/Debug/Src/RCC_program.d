ba1de8bededae150b066e79f7b050586f8556612
KarimMagdy <kmagdyashour@gmail.com>
2022-11-04 20:12:04 +0200

    first commit

---
+++ 01-RCC/Debug/Src/RCC_program.d
@@ -0,0 +1,6 @@
+Src/RCC_program.o: ../Src/RCC_program.c ../Inc/../Inc/STD_TYPES.h \
+ ../Inc/STM32F446xx.h ../Inc/../Inc/RCC_Int.h ../Inc/../Inc/RCC_priv.h
+../Inc/../Inc/STD_TYPES.h:
+../Inc/STM32F446xx.h:
+../Inc/../Inc/RCC_Int.h:
+../Inc/../Inc/RCC_priv.h:
---
+++ 01-RCC/Debug/Src/main.d
@@ -0,0 +1,5 @@
+Src/main.o: ../Src/main.c ../Src/../Inc/STD_TYPES.h \
+ ../Src/../Inc/STM32F446xx.h ../Src/../Inc/RCC_Int.h
+../Src/../Inc/STD_TYPES.h:
+../Src/../Inc/STM32F446xx.h:
+../Src/../Inc/RCC_Int.h:
